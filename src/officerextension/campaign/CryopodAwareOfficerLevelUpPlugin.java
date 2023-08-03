5860ec042c4ebdd240e7a6ebeb3a020f9e8bbb23
5thYL <35957580+5thYL@users.noreply.github.com>
2023-08-04 07:48:41 +0800

    Add files via upload

--- src/officerextension/campaign/CryopodAwareOfficerLevelUpPlugin.java
+++ src/officerextension/campaign/CryopodAwareOfficerLevelUpPlugin.java
@@ -4,12 +4,25 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.characters.PersonAPI;
 import com.fs.starfarer.api.impl.campaign.OfficerLevelupPluginImpl;
 import com.fs.starfarer.api.impl.campaign.ids.MemFlags;
+import officerextension.Settings;
 
 public class CryopodAwareOfficerLevelUpPlugin extends OfficerLevelupPluginImpl {
     @Override
     public int getMaxLevel(PersonAPI person) {
         if (person != null && person.getMemoryWithoutUpdate().getBoolean(MemFlags.EXCEPTIONAL_SLEEPER_POD_OFFICER)) {
-            return Global.getSettings().getInt("exceptionalSleeperPodsOfficerLevel");
+            if (person.getMemoryWithoutUpdate().getInt(Settings.OFFICER_LEVEL_CAP) <= Global.getSettings().getInt("exceptionalSleeperPodsOfficerLevel")) {
+                return Global.getSettings().getInt("exceptionalSleeperPodsOfficerLevel");
+            }
+            if (person.getMemoryWithoutUpdate().getInt(Settings.OFFICER_LEVEL_CAP) > Global.getSettings().getInt("exceptionalSleeperPodsOfficerLevel")) {
+                return person.getMemoryWithoutUpdate().getInt(Settings.OFFICER_LEVEL_CAP);
+            }
+
+        }
+        if (person != null && person.getMemoryWithoutUpdate().getInt(Settings.OFFICER_LEVEL_CAP) <= super.getMaxLevel(person)) {
+            return super.getMaxLevel(person);
+        }
+        if (person != null && person.getMemoryWithoutUpdate().getInt(Settings.OFFICER_LEVEL_CAP) > super.getMaxLevel(person)) {
+            return person.getMemoryWithoutUpdate().getInt(Settings.OFFICER_LEVEL_CAP);
         }
         return super.getMaxLevel(person);
     }
